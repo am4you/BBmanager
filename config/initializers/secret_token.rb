93edd501a5c7ce2a48d41c5526638a8bca0e1bee
am4you <info@am4you.net>
2013-01-07 11:01:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BBmanager::Application.config.secret_token = 'b62e82f03b0473e6e431f96ce5a51b684e83d5615d516d58252a8687faae5e679bb1b15438121fa49f6227788572385a1f6eb4eb99805b227c240d606d5e5309'
