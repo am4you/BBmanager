0868371698ab866ee660c08db4f85d61dd5d9bfb
am4you <info@am4you.net>
2013-01-07 11:12:10 +0100

    Controller static pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BBmanager::Application.routes.draw do
+  get "static_pages/index"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
