9fa2c7137a76e287e442f0636d994aecf3b8dab0
am4you <info@am4you.net>
2013-01-07 18:33:49 +0100

    Registrazione utente con mail di conferma

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,19 @@ BBmanager::Application.configure do
 
   # Manual setup
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    require 'tlsmail'    
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "info@am4you.net",
+    :password => "magerose82",
+  }
 end
