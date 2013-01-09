4551c315942c8c64f92094a7bb108052f469508e
am4you <info@am4you.net>
2013-01-09 20:07:07 +0100

    Header menu updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :surname
   # attr_accessible :title, :body
 
   has_one :bb
